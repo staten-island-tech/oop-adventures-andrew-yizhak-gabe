4f60910066743f4d27021563de3945b65b7ca7e7
Izzy-Zoltan <izakzoltan@gmail.com>
2023-05-03 08:39:13 -0400

    5/3
    Some errors need to be fixed.

--- blackjackgame.py
+++ blackjackgame.py
@@ -4,16 +4,16 @@ import random
 playershand=[]
 opponentshand=[]
 cardsdrawn=0
-random.shuffle(blackjack)
+random.shuffle(blackjack.deck)
 
-def draw(card,x):
+def draw(card,x,cardsdrawn):
     if cardsdrawn==51:
         cardsdrawn=0
-        random.shuffle(blackjack)
+        random.shuffle(blackjack.deck)
     for cards in range(x):
         card.append(blackjack(cardsdrawn))
         cardsdrawn+=1
 
-draw(playershand,2)
+draw(playershand,2,cardsdrawn)
 print('Welcome to the blackjack BETA! Here, this is your starting hand!' + {playershand})
 
