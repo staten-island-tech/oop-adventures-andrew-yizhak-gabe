94dc430a8a1e7f90e6d28f51812d6a300a48e52f
Izzy-Zoltan <izakzoltan@gmail.com>
2023-05-02 21:16:20 -0400

    5/2
    done most of the cards up to Jack of Gabriel Liberov; Future planning regarding Ace.

---
+++ blackjack.py
@@ -0,0 +1,26 @@
+import random
+
+deck=[{'Ace of Michael Whalen':''},{'Ace of Gabriel Liberov':''},{'Ace of Andrew Rosini':''},{'Ace of Izzy Zoltan':''},
+      {'Two of Michael Whalen':2},{'Two of Gabriel Liberov':2},{'Two of Andrew Rosini':2},{'Two of Izzy Zoltan':2},
+      {'Three of Michael Whalen':3},{'Three of Gabriel Liberov':3},{'Three of Andrew Rosini':3},{'Three of Izzy Zoltan':3},
+      {'Four of Michael Whalen':4},{'Four of Gabriel Liberov':4},{'Four of Andrew Rosini':4},{'Four of Izzy Zoltan':4},
+      {'Five of Michael Whalen':5},{'Five of Gabriel Liberov':5},{'Five of Andrew Rosini':5},{'Five of Izzy Zoltan':5},
+      {'Six of Michael Whalen':6},{'Six of Gabriel Liberov':6},{'Six of Andrew Rosini':6},{'Six of Izzy Zoltan':6},
+      {'Seven of Michael Whalen':7},{'Seven of Gabriel Liberov':7},{'Seven of Andrew Rosini':7},{'Seven of Izzy Zoltan':7},
+      {'Eight of Michael Whalen':8},{'Eight of Gabriel Liberov':8},{'Eight of Andrew Rosini':8},{'Eight of Izzy Zoltan':8},
+      {'Nine of Michael Whalen':9},{'Nine of Gabriel Liberov':9},{'Nine of Andrew Rosini':9},{'Nine of Izzy Zoltan':9},
+      {'Ten of Michael Whalen':10},{'Ten of Gabriel Liberov':10},{'Ten of Andrew Rosini':10},{'Ten of Izzy Zoltan':10},
+      {'Jack of Michael Whalen':10},{'Jack of Gabriel Liberov':10},{'Ten of Andrew Rosini':10},{'Ten of Izzy Zoltan':10},
+      ]
+
+""""
+THE BANE OF MY EXISTENCE - ACE:
+
+OPTION 1:
+ Let the computer choose if it's 1 or 11 at the end of the turn using math. 
+ Option 2: let the player choose if it's 1 or 11 at the end of the turn using an input and if function
+
+ We're going to need a threshold for the npc to stop drawing cards. 
+"""
+#notetoself: 1,2,3,4,5,6,7,8,9,10,jack,queen,king
+#gabriel liberov Izzy Zoltan Andrew Rosini Michael Whalen 
