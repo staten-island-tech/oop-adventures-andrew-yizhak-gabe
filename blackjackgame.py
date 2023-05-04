38214a56d481a7c297d20c65caaa380f90ae0ca1
Izzy-Zoltan <izakzoltan@gmail.com>
2023-05-04 08:15:01 -0400

    5/4
    Draw function finished: Work needed on the print function to only print the keys.

--- blackjackgame.py
+++ blackjackgame.py
@@ -6,14 +6,17 @@ opponentshand=[]
 cardsdrawn=0
 random.shuffle(blackjack.deck)
 
-def draw(card,x,cardsdrawn):
+def draw(card,x):
+    global cardsdrawn
+
     if cardsdrawn==51:
         cardsdrawn=0
         random.shuffle(blackjack.deck)
     for cards in range(x):
-        card.append(blackjack(cardsdrawn))
+        card.append(blackjack.deck[cardsdrawn])
         cardsdrawn+=1
 
-draw(playershand,2,cardsdrawn)
-print('Welcome to the blackjack BETA! Here, this is your starting hand!' + {playershand})
+draw(playershand,2)
+print(f'Welcome to the blackjack BETA! Here, this is your starting hand! {playershand}')
+
 
