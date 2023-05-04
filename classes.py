a8022a391f07f36905d6acde1f4325872b9001ae
Andrew-Rosini <124058279+Andrew-Rosini@users.noreply.github.com>
2023-05-04 08:31:26 -0400

    May 4th - Step 1
    Created the base classes, need to continue refining their development tomorrow.

--- classes.py
+++ classes.py
@@ -1,16 +1,33 @@
-class Character:
-    def __init__(self, name, health):
-        self.name = name
-        self.health = health 
-class Player(Character):
-    def __init__(self, name, health):
-        super().__init__(self, name, health)
-class NPC(Character):
+import uuid
+class User:
     def __init__(self, name):
-        super().__init__(self, name)
-class Enemy(Character):
+        self.name = name
+    new_id = str(uuid.uuid4())
+
+class Enemy(User):
     def __init__(self, name, health):
-        super().__init__(self, name, health)
-class Boss(Enemy):
-    def __init__(self, name, health):
-        super().__init__(self, name, health)
+        super().__init__(name)
+        self.health = health
+    def __str__(self):
+        return f"{self.name}, {self.health}"
+
+class Henchmen(Enemy):
+    def __init__(self, name, health, hand):
+        super().__init__(name, health)
+        self.hand = hand
+    def __str__(self):
+        return f"{self.name}, {self.health}, {self.hand}"
+
+class Enforcer(Enemy):
+    def __init__(self, name, health, hand):
+        super().__init__(name, health)
+        self.hand = hand
+    def __str__(self):
+        return f"{self.name}, {self.health}, {self.hand}"
+
+class Boss(Enforcer):
+    def __init__(self, name, health, hand, region):
+        super().__init__(name, health, hand)
+        self.region = region
+    def __str__(self):
+        return f"{self.name}, {self.health}, {self.hand}, {self.region}"
