3854e1fce7b2a26291e304b931132b1d6eb2fe54
gliberov <124058342+gliberov@users.noreply.github.com>
2023-05-04 08:29:55 -0400

    Star Wars

--- classes.py
+++ classes.py
@@ -1,7 +1,9 @@
+import uuid
 class Character:
     def __init__(self, name, health):
         self.name = name
-        self.health = health 
+        self.health = health
+    new_id = str(uuid.uuid1()) 
 class Player(Character):
     def __init__(self, name, health):
         super().__init__(self, name, health)
