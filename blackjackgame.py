57bc63119311ac4acb3e56d2acca6c512efe7744
Izzy-Zoltan <izakzoltan@gmail.com>
2023-05-04 08:29:57 -0400

    5/4 2
    notes added for self; Wip on drawing more.

--- blackjackgame.py
+++ blackjackgame.py
@@ -19,4 +19,17 @@ def draw(card,x):
 draw(playershand,2)
 print(f'Welcome to the blackjack BETA! Here, this is your starting hand! {playershand}')
 
+draw(opponentshand,2)
+drawmore=input(f'Your dealer has the {opponentshand[0]} and one facedown card. Do you want to draw? |Y/N|').upper()[0]
 
+while drawmore == 'Y':
+    """
+    calculate card value (have fun calcing ace)
+
+    if handvalue > 21: 
+        print('you lose')
+    else:
+        ask user if they want to drawmore
+        
+    
+    """
