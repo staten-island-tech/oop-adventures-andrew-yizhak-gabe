041b2e914e74bfaac440bf67ce45ddb696ca8615
Andrew-Rosini <124058279+Andrew-Rosini@users.noreply.github.com>
2023-05-05 08:11:02 -0400

    May 5th - Step 1
    Created functions that will make new of each class needed for the game. Need to add filler data for the functions.

--- classes.py
+++ classes.py
@@ -11,7 +11,7 @@ class Enemy(User):
     def __str__(self):
         return f"{self.name}, {self.health}"
 
-class Henchmen(Enemy):
+class Henchman(Enemy):
     def __init__(self, name, health, hand):
         super().__init__(name, health)
         self.hand = hand
@@ -30,4 +30,33 @@ class Boss(Enforcer):
         super().__init__(name, health, hand)
         self.region = region
     def __str__(self):
-        return f"{self.name}, {self.health}, {self.hand}, {self.region}"
+        return f"{self.name}, {self.health}, {self.hand}"
+
+enemies = []
+henchmen = []
+enforcers = []
+bosses = []
+
+def create_new_enemy(name, health):
+    new_enemy = Enemy(name, health)
+    enemies.append(new_enemy)
+    for enemy in enemies:
+        print(enemy)
+
+def create_new_henchman(name, health, hand):
+    new_henchman = Henchman(name, health, hand)
+    henchmen.append(new_henchman)
+    for henchman in henchmen:
+        print(henchman)
+
+def create_new_enforcer(name, health, hand):
+    new_enforcer = Enforcer(name, health, hand)
+    enforcers.append(new_enforcer)
+    for enforcer in enforcers:
+        print(enforcer)
+
+def create_new_boss(name, health, hand, region):
+    new_boss = Boss(name, health, hand, region)
+    bosses.append(new_boss)
+    for boss in bosses:
+        print(boss)
