8af9635692229cc9bdfeaa8cbe99dc5382a8b4d9
Andrew-Rosini <124058279+Andrew-Rosini@users.noreply.github.com>
2023-05-11 08:34:38 -0400

    May 11th - Step 1
    Created the player hand function for the blackjack game.

--- blackjackgame.py
+++ blackjackgame.py
@@ -4,10 +4,39 @@ import random
 players_hand = list()
 opponents_hand = list()
 
-random_suit = print(random.choice(blackjack.deck_suits))
-random_value = print(random.choice(blackjack.deck_values))
+def player_cards():
+    cards = [2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11]
+    
+    face_up_value = random.choice(cards)
+    if face_up_value == 10:
+        possible_cards = ["10", "Jack", "Queen", "King"]
+        random_card = random.choice(possible_cards)
+        suits = ["Michael Whalen", "Gabriel Liberov", "Izzy Zoltan", "Andrew Rosini"]
+        random_suit = random.choice(suits)
+        face_up_card = (f"Your faceup card is the {random_card} of {random_suit}")
+    elif face_up_value < 10:
+        suits = ["Michael Whalen", "Gabriel Liberov", "Izzy Zoltan", "Andrew Rosini"]
+        random_suit = random.choice(suits)
+        face_up_card = (f"Your faceup card is the {face_up_value} of {random_suit}")
+    elif face_up_value == 11:
+        cards.remove(11)
+        face_up_card = (f"Your faceup card is the ace of {random_suit}")
+    players_hand.append(face_up_value)
 
-if random_value == 10:
-    deck_faces = ["Ten", "Jack", "Queen", "King"]
-    random_face = random.choice(deck_faces)
-    print(f"{random_face} of {random_suit}")
+    face_down_value = random.choice(cards)
+    if face_down_value == 10:
+        possible_cards = ["10", "Jack", "Queen", "King"]
+        random_card = random.choice(possible_cards)
+        suits = ["Michael Whalen", "Gabriel Liberov", "Izzy Zoltan", "Andrew Rosini"]
+        random_suit = random.choice(suits)
+        face_down_card = (f"your facedown card is the {random_card} of {random_suit}.")
+    elif face_down_value != 10:
+        suits = ["Michael Whalen", "Gabriel Liberov", "Izzy Zoltan", "Andrew Rosini"]
+        random_suit = random.choice(suits)
+        face_down_card = (f"your facedown card is the {face_down_value} of {random_suit}.")
+    players_hand.append(face_down_value)
+
+    print(f"{face_up_card}, and {face_down_card}")
+    if face_up_value == 11 and random_card == "Jack":
+        print("Blackjack! You get an automatic win!")
+player_cards()
