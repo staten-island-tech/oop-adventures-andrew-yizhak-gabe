14634c4eafe893e67c41a5d69b3e731e687ae81b
Andrew-Rosini <124058279+Andrew-Rosini@users.noreply.github.com>
2023-05-13 12:13:12 -0400

    May 13th - Step 1
    Completed approximately 95% of the hit function. Still need to repeat the function itself.

--- blackjackgame.py
+++ blackjackgame.py
@@ -1,11 +1,15 @@
 import blackjack 
 import random
+from time import sleep
 
 players_hand = list()
 opponents_hand = list()
+cards = [2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11]
+suits = ["Michael Whalen", "Gabriel Liberov", "Izzy Zoltan", "Andrew Rosini"]
 
 def player_cards():
     cards = [2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11]
+    suits = ["Michael Whalen", "Gabriel Liberov", "Izzy Zoltan", "Andrew Rosini"]
     
     face_up_value = random.choice(cards)
     if face_up_value == 10:
@@ -19,6 +23,7 @@ def player_cards():
         random_suit = random.choice(suits)
         face_up_card = (f"Your faceup card is the {face_up_value} of {random_suit}")
     elif face_up_value == 11:
+        random_suit = random.choice(suits)
         cards.remove(11)
         face_up_card = (f"Your faceup card is the ace of {random_suit}")
     players_hand.append(face_up_value)
@@ -37,12 +42,13 @@ def player_cards():
     players_hand.append(face_down_value)
 
     print(f"{face_up_card}, and {face_down_card}")
-    if face_up_value == 11 and random_card == "Jack":
-        print("Blackjack! You get an automatic win!")
 player_cards()
+
+sleep(2.5)
 
 def opponents_card():
     cards = [2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11]
+    suits = ["Michael Whalen", "Gabriel Liberov", "Izzy Zoltan", "Andrew Rosini"]
     
     face_up_value = random.choice(cards)
     if face_up_value == 10:
@@ -56,6 +62,7 @@ def opponents_card():
         random_suit = random.choice(suits)
         face_up_card = (f"Your opponent's deck consists of a faceup card, which is the {face_up_value} of {random_suit}")
     elif face_up_value == 11:
+        random_suit = random.choice(suits)
         cards.remove(11)
         face_up_card = (f"Your opponent's deck consists of a faceup card, which is the ace of {random_suit}")
     opponents_hand.append(face_up_value)
@@ -74,45 +81,33 @@ def opponents_card():
     opponents_hand.append(face_down_value)
 
     print(f"{face_up_card}, and one facedown card.")
-    if face_up_value == 11 and random_card == "Jack":
-        print("Blackjack! Your opponent gets an automatic win!")
 opponents_card()
 
+sleep(2.5)
 
-
-
-
-
-
-
-
-
-
-
-def player_draw():
-    i = 0.1
-    while i != 0:
-        hit_or_stand = input("Would you like to hit or stand? HIT/STAND: ")
-        if hit_or_stand == "HIT":
-            cards = [2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11]
-            new_card = random.choice(cards)
-            if new_card == 11:
-                current_sum = sum(players_hand)
-                if current_sum + 11 > 21:
-                    new_card == 1
-                    players_hand.append(new_card)
-                else:
-                    new_card = input("Do you want your ace to be valued at 1 or 11? 1/11: ")
-            else:
-                current_sum = sum(players_hand)
-                new_sum = current_sum + new_card
-                if new_sum > 21:
-                    print("Bust! You lose!")
-                    i = 0
-                else:
-                    print(new_sum)
-        elif hit_or_stand == "STAND":
-            card_sum = sum(players_hand)
-            print(f"The total sum of your cards is {card_sum}.")
-            i = 0
-player_draw()
+hit_or_stand = input("Would you like to hit or stand? Hit/Stand: ")
+if hit_or_stand == "Hit":
+    new_card_value = random.choice(cards)
+    if new_card_value == 10:
+        possible_cards = ["10", "Jack", "Queen", "King"]
+        random_card = random.choice(possible_cards)
+        suits = ["Michael Whalen", "Gabriel Liberov", "Izzy Zoltan", "Andrew Rosini"]
+        random_suit = random.choice(suits)
+        new_card = (f"Your new card is the {random_card} of {random_suit}.")
+    elif new_card_value < 10:
+        suits = ["Michael Whalen", "Gabriel Liberov", "Izzy Zoltan", "Andrew Rosini"]
+        random_suit = random.choice(suits)
+        new_card = (f"Your new card is the {new_card_value} of {random_suit}.")
+    elif new_card_value == 11:
+        random_suit = random.choice(suits)
+        new_card = (f"Your new card is the ace of {random_suit}.")
+        if sum(players_hand) + 11 > 21:
+            new_card_value = 1
+        elif sum(players_hand) + 11 < 21:
+            new_card_value = input("Would you like your ace to be valued as a 1 or 11? 1/11: ")
+            if new_card_value == "1":
+                new_card_value = 1
+            elif new_card_value == "11":
+                new_card_value = 11
+    players_hand.append(new_card_value)
+    players_sum = sum(players_hand)
