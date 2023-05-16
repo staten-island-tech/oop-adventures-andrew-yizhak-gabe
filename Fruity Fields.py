ab3509376ae64e4d7aff9bff5adb3a51a9f1c22c
Izzy-Zoltan <izakzoltan@gmail.com>
2023-05-16 08:39:34 -0400

    5/16

---
+++ Fruity Fields.py
@@ -0,0 +1,5 @@
+from time import sleep
+
+print('You exit out of the bar, and head towards the luscious FRUITY FIELDS. \'Remember Megaprimatus,\' you think to yourself, \'it\'s all for Gudrun Norberto Porvy-Wulfhard...\'')
+sleep(3)
+print('The grass is ')
