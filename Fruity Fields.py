09210a167c5f632eb5b199f3e4225c622205889c
Izzy-Zoltan <izakzoltan@gmail.com>
2023-05-17 08:05:02 -0400

    5/17

--- Fruity Fields.py
+++ Fruity Fields.py
@@ -1,5 +1,7 @@
 from time import sleep
 
-print('You exit out of the bar, and head towards the luscious FRUITY FIELDS. \'Remember Megaprimatus,\' you think to yourself, \'it\'s all for Gudrun Norberto Porvy-Wulfhard...\'')
+print('Narrator: King D. Kong and Owen Zhao embarked out of the bar, and into the luscious Fruity Fields.')
 sleep(3)
-print('The grass is ')
+print('Owen: Owen giggles stupidly.\"Gee wiz Kongy! I\'ve always wondered, why are you doing this anyways? DUIH!\" ')
+sleep(3)
+print('')
