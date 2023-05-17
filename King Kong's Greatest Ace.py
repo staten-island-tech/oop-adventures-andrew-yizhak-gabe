d62fb713fcf1fdc129be107754af795fcf9546c1
Andrew-Rosini <124058279+Andrew-Rosini@users.noreply.github.com>
2023-05-17 08:20:15 -0400

    May 17th - Step 2
    Wrote a to-do list to keep track of what is left to write.

--- King Kong's Greatest Ace.py
+++ King Kong's Greatest Ace.py
@@ -1,1 +1,8 @@
 import tutorial
+
+"""
+Left to-do:
+- Finish writing the storyline.
+- Code in the shop and other parts of the map.
+- Create a system for health and other assorted mechanics, including the henchmen and enforcers.
+"""
