aadab8f3f37042011deb702e7a222035a07128ab
Izzy-Zoltan <izakzoltan@gmail.com>
2023-05-24 08:38:58 -0400

    5/24

--- blackjack.py
+++ blackjack.py
@@ -1,45 +1,54 @@
 import random
 from time import sleep
-    
+
 players_hand = []
 opponents_hand = []
 cards = [2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11]
 Tens=['10','Jack','Queen','King']
 suits = ["Michael Whalen", "Gabriel Liberov", "Izzy Zoltan", "Andrew Rosini"]
     
-face_up_value = random.choice(cards)
-face_down_value = random.choice(cards)
+cardValue = random.choice(cards)
+class game():
+    def card(facing,cardValue,hand,player):
+        
+        cardValue = random.choice(cards)
+        if cardValue == 10:
+            return(f"{player} {facing} card is the {random.choice(Tens)} of {random.choice(suits)}")
+        elif cardValue < 10:
+            return(f"{player} {facing} card is the {cardValue} of {random.choice(suits)}")
+        elif cardValue == 11:
+            if player=='player':
+                if sum(hand) + 11 > 21:
+                    cardValue = 1
+                elif sum(hand) + 11 < 21:
+                    cardValue = input("Would you like your ace to be valued as a 1 or 11? 1/11: ")
+                    if cardValue == "1":
+                        cardValue = 1
+                    elif cardValue == "11":
+                        cardValue = 11
+            else:
+                if sum(players_hand) + 11 > 21:
+                    cardValue = 1
+                elif sum(players_hand) + 11 < 21:
+                    cardValue = 11
+                return(f"{player} {facing} card is the ace of {random.choice(suits)}")
+        return(cardValue)
+        
+face_up_card=game.card('face up',cardValue,players_hand,'player')
+players_hand.append(cardValue)
+print(players_hand)
+face_down_card=game.card('face down',cardValue,players_hand,'player')
+players_hand.append(cardValue)
+print(players_hand)
 
-def card(facing,facingvalue):
-    if facingvalue == 10:
-        return(f"Your {facing} card is the {random.choice(Tens)} of {random.choice(suits)}")
-    elif face_up_value < 10:
-        return(f"Your {facing} card is the {face_up_value} of {random.choice(suits)}")
-    elif facingvalue == 11:
-        cards.remove(11)
-        return(f"Your {facing} card is the ace of {random.choice(suits)}")
-    players_hand.append(facingvalue)
-
-face_up_card=card('face up',face_up_value)
-face_down_card=card('face down',face_down_value)
-
-print(f"{face_up_card}, and {face_down_card}")
+print(f"{face_up_card}, and {face_down_card}.")
 
 sleep(2.5)
 
-face_up_value = random.choice(cards)
-if face_up_value == 10:
-
-    face_up_card = (f"Your opponent's deck consists of a faceup card, which is the {random.choice(Tens)} of {random.choice(suits)}")
-elif face_up_value < 10:
-    face_up_card = (f"Your opponent's deck consists of a faceup card, which is the {face_up_value} of {random.choice(suits)}")
-elif face_up_value == 11:
-    cards.remove(11)
-    face_up_card = (f"Your opponent's deck consists of a faceup card, which is the ace of {random.choice(suits)}")
-opponents_hand.append(face_up_value)
-
-face_down_value = random.choice(cards)
-opponents_hand.append(face_down_value)
+face_up_card=game.card('face up',cardValue,opponents_hand,'opponent')
+opponents_hand.append(cardValue)
+cardValue = random.choice(cards)
+opponents_hand.append(cardValue)
 
 print(f"{face_up_card}, and one facedown card.")
 
@@ -47,23 +56,8 @@ sleep(2.5)
 
 hit_or_stand = input("Would you like to hit or stand? Hit/Stand: ")
 while hit_or_stand == "Hit":
-    new_card_value = random.choice(cards)
-    card('new',new_card_value)
-    if new_card_value == 10:
-        new_card = (f"Your new card is the {random.choice(Tens)} of {random.choice(suits)}.")
-    elif new_card_value < 10:
-        new_card = (f"Your new card is the {new_card_value} of {random.choice(suits)}.")
-    elif new_card_value == 11:
-        new_card = (f"Your new card is the ace of {random.choice(suits)}.")
-        if sum(players_hand) + 11 > 21:
-            new_card_value = 1
-        elif sum(players_hand) + 11 < 21:
-            new_card_value = input("Would you like your ace to be valued as a 1 or 11? 1/11: ")
-            if new_card_value == "1":
-                new_card_value = 1
-            elif new_card_value == "11":
-                new_card_value = 11
-    players_hand.append(new_card_value)
+    new_card=game.card('new card',cardValue,players_hand)
+    
     players_sum = sum(players_hand)
     print(new_card)
     if players_sum > 21:
@@ -81,22 +75,22 @@ while player_position == players_sum:
     if opponents_sum > players_sum:
         print(f"Your opponents has gained a higher score than you, {opponents_sum}. You will now lose the number of hearts you gambled at the beginning of the round.")
         break
-    new_card_value = random.choice(cards)
-    if new_card_value == 10:
+    cardValue = random.choice(cards)
+    if cardValue == 10:
         new_card = (f"Your new card is the {random.choice(Tens)} of {random.choice(suits)}.")
-    elif new_card_value < 10:
-        new_card = (f"Your new card is the {new_card_value} of {random.choice(suits)}.")
-    elif new_card_value == 11:
+    elif cardValue < 10:
+        new_card = (f"Your new card is the {cardValue} of {random.choice(suits)}.")
+    elif cardValue == 11:
         new_card = (f"Your new card is the ace of {random.choice(suits)}.")
         if sum(players_hand) + 11 > 21:
-            new_card_value = 1
+            cardValue = 1
         elif sum(players_hand) + 11 < 21:
-            new_card_value = input("Would you like your ace to be valued as a 1 or 11? |1|11|: ")
-            if new_card_value == "1":
-                new_card_value = 1
-            elif new_card_value == "11":
-                new_card_value = 11
-    opponents_hand.append(new_card_value)
+            cardValue = input("Would you like your ace to be valued as a 1 or 11? |1|11|: ")
+            if cardValue == "1":
+                cardValue = 1
+            elif cardValue == "11":
+                cardValue = 11
+    opponents_hand.append(cardValue)
     opponents_sum = sum(opponents_hand)
     if opponents_sum > 21:
         print("Your opponent has busted. You have defeated your oppenent, and absorbed half of their hearts.")
