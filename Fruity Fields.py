59f270453f4b078dd9ce0a26d826db91f4900eb3
Andrew-Rosini <124058279+Andrew-Rosini@users.noreply.github.com>
2023-05-27 09:57:17 -0400

    May 27th - Step 1
    Made progress with the middle game fights, but need to edit the health system.

--- Fruity Fields.py
+++ Fruity Fields.py
@@ -1,5 +1,8 @@
 from time import sleep
 from classes import Lil_Nas_X
+from classes import henchman
+from classes import enforcer
+from itertools import repeat
 count = []
 delay = 3
 health = 100
@@ -232,8 +235,10 @@ if opponents_sum > 21:
     elif count == 0:
         new_health = health
 
+health == new_health
+count.clear()
 
-print(f"Narrator: Your health is now {new_health}. You will need to survive the rest of this level with {new_health} health.")
+print(f"Narrator: Your health is now {health}. You will need to survive the rest of this level with {health} health.")
 sleep(delay)
 print("Lil Nas X: Okay, so you said you need to bring about the drums of liberation. How exactly do you plan to take down the bird mafia?")
 sleep(delay)
@@ -245,5 +250,66 @@ print("King Kong: Thank you. I need to go pay this Bobby Hill a visit.")
 sleep(delay)
 print("Australian Henriques: See ya later, yank.")
 sleep(delay)
+print("Narrator: You and Australian Henriques stroll out of the Seer's house, but when you get a few blocks away, a short, stocky man beings approaching you. He is carrying a baton made out of a long, powerful looking feather.")
+sleep(delay)
+print("Australian Henriques: Kong... get behind me...")
+sleep(delay)
+print("Narrator: You dart behind Australian Henriques just as the stocky man reaches you, at which point you realize he is local law enforcement.")
+sleep(delay)
+print("Henchman: Hello boys, how are you today?")
+sleep(delay)
+print("Australian Henriques: Alright, but these damn Yanks are acting crazy.")
+sleep(delay)
+print("Narrator: At this point, the henchman holds up a picture of King Kong.")
+sleep(delay)
+print("Henchman: You boys wouldn't have happened to see this monkey around here, have you? Because he is wanted by Bird Mafia.")
+sleep(delay)
+print("King Kong: I'm an ape, damn it!")
+sleep(delay)
+print("Narrator: You grab the henchman by his uniform, chucking him across the way. However, he is unfazed as you didn't damage him using the power of blackjack.")
+sleep(delay)
+print("Narrator: The henchman then pulls out his walkie talkie and calls for backup, before advancing on you himself.")
+sleep(delay)
+print("Henchman: Kong... you are going to regret pulling that maneuver.")
+sleep(delay)
+print("King Kong: Squawk, squawk then birdie... let's do this.")
+sleep(delay)
+print(henchman)
+sleep(delay)
+gamble = int(input(f"Narrator: How much health would you like to gamble on the fight? You currently have {health} health: "))
 
-#Now up to adding in the story, kill some henchmen, and fight Tweety.#
+def henchman_fight():
+    blackjack()
+    while players_sum > 21:
+        print("Narrator: The henchman wacks you with his baton, grinning as he slowly defeats you.")
+        new_health = health - gamble
+        count.append(1)
+        if new_health <= 0:
+            print("You are out of health, and your game has ended.")
+            break
+        blackjack()
+    while players_sum < opponents_sum and opponents_sum < 21:
+        print("Narrator: The henchman wacks you with his baton, grinning as he slowly defeats you.")
+        new_health = health - gamble
+        count.append(1)
+        if new_health <= 0:
+            print("You are out of health, and your game has ended.")
+            break
+        blackjack()
+    if opponents_sum > 21:
+        if count != 0:
+            new_health = health - ((len(count)) * gamble)
+        elif count == 0:
+            new_health = health
+        henchman_health = 30 - gamble
+        if henchman_health <= 0:
+            print("King Kong: Bam. Scumbag down.")
+            sleep(delay)
+            return health and new_health
+        elif henchman_health > 0:
+            print("Australian Henriques: Watch out, this Yank's coming back for more.")
+            sleep(delay)
+            print(f"The henchman you are facing is now on {henchman_health}.")
+henchman_fight()
+
+#Next up: force the henchman fight to repeat, make fight scene, make boss fight, then done with Fruity Fields.#
